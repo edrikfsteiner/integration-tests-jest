0a748b210bf154c6ae56c175fcfad85a8062ae1c
edrikfsteiner <edrikfsteiner@gmail.com>
2025-09-23 00:10:48 -0300

    first api test

---
+++ test/army.spec.ts
@@ -0,0 +1,51 @@
+import pactum from 'pactum';
+import { StatusCodes } from 'http-status-codes';
+import { SimpleReporter } from '../simple-reporter';
+
+describe('Army API Test', () => {
+  const p = pactum;
+  const rep = SimpleReporter;
+  const baseUrl = 'http://localhost:8080';
+
+  p.request.setDefaultTimeout(10000);
+
+  beforeAll(() => {
+    p.reporter.add(rep);
+
+    return p
+      .spec()
+      .post(`${baseUrl}/countries`)
+      .withJson({
+        name: 'País de Teste para Exércitos'
+      })
+      .expectStatus(StatusCodes.OK)
+      .stores('CountryId', 'id');
+  });
+
+  afterAll(() => {
+    p.reporter.end();
+  });
+
+  describe('Armies', () => {
+    const armyName = 'Exército de Teste';
+    const updatedArmyName = 'Exército de Teste Atualizado';
+
+    it('should create a new army', async () => {
+      await p
+        .spec()
+        .post(`${baseUrl}/armies`)
+        .withJson({
+          name: armyName,
+          country: {
+            id: '$S{CountryId}'
+          },
+          divisions: []
+        })
+        .expectStatus(StatusCodes.OK)
+        .expectJsonLike({
+          name: armyName
+        })
+        .stores('ArmyId', 'id');
+    });
+  });
+});
